676e7b3c53aff8c8fb7b28e80fd4a994128bb085
I504428 <gabriel.azambuja@sap.com>
2025-02-14 18:41:10 -0300

    feat: add navbar

--- frontend/components/main-nav.tsx
+++ frontend/components/main-nav.tsx
@@ -12,29 +12,26 @@ const MainNav = () => {
       label: "Home",
       // TODO: highlight when click later..
     },
-    { href: "/create-event", label: "Create Event" }, // restrict to non organizers
+    { href: "/create-event", label: "Create Event" }, // restrict to only organizers
     { href: "/my-tickets", label: "My Tickets" },
     { href: "/marketplace", label: "Marketplace" },
-    { href: "/about", label: "About" },
-    { href: "/contact", label: "Contact" },
   ];
+
   return (
-    <div>
-      <nav className="p-6 h-20 border-b border-b-slate-300 flex">
-        <div className="p-4 flex items-center mr-3 font-bold text-2xl">
-          <h2>🎟️ NFT Ticketing dApp</h2>
-        </div>
-        <div className="flex justify-center text-lg">
-          <ul className="flex items-center">
-            {routes.map((item) => (
-              <li key={item.label} className="ml-6">
-                <Link href={item.href}>{item.label}</Link>
-              </li>
-            ))}
-          </ul>
-        </div>
-      </nav>
-    </div>
+    <nav className="p-6 h-20 border-b border-b-slate-300 flex">
+      <div className="p-4 flex items-center mr-3 font-bold text-2xl">
+        <h2 className="">🎟️ NFT Ticketing dApp</h2>
+      </div>
+      <div className="flex justify-center text-xl">
+        <ul className="flex items-center space-x-4">
+          {routes.map((item) => (
+            <li key={item.label}>
+              <Link href={item.href}>{item.label}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </nav>
   );
 };
 
