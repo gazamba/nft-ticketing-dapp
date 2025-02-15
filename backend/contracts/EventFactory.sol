4e9d065c340070d1d56244020f53856348235284
I504428 <gabriel.azambuja@sap.com>
2025-02-15 18:25:26 -0300

    feat: add description event field

--- backend/contracts/EventFactory.sol
+++ backend/contracts/EventFactory.sol
@@ -5,6 +5,7 @@ contract EventFactory {
     struct Event {
         uint256 eventId;
         string name;
+        string description;
         string date;
         string location;
         uint256 ticketPrice;
@@ -15,16 +16,30 @@ contract EventFactory {
     uint256 private nextEventId;
     mapping(uint256 => Event) public events;
 
-    event EventCreated(uint256 indexed eventId, string name, address indexed organizer);
+    event EventCreated(
+        uint256 indexed eventId,
+        string name,
+        address indexed organizer
+    );
 
     function createEvent(
         string memory _name,
+        string memory _description,
         string memory _date,
         string memory _location,
         uint256 _ticketPrice,
         uint256 _totalTickets
     ) external {
-        events[nextEventId] = Event(nextEventId, _name, _date, _location, _ticketPrice, _totalTickets, msg.sender);
+        events[nextEventId] = Event(
+            nextEventId,
+            _name,
+            _description,
+            _date,
+            _location,
+            _ticketPrice,
+            _totalTickets,
+            msg.sender
+        );
         emit EventCreated(nextEventId, _name, msg.sender);
         nextEventId++;
     }
