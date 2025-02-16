e5f119ce118295f25fda32386da2cbfb2f16646c
I504428 <gabriel.azambuja@sap.com>
2025-02-16 20:07:44 -0300

    feat: add button router

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -8,54 +10,55 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import Heading from "@/components/ui/heading";
-
-const events = [
-  {
-    id: 1,
-    name: "Web 3 Conference",
-    description: "A conference about the future of Web 3.0 technologies.",
-    date: "2025-06-10",
-    location: "Atlantis the Palm, Dubai",
-    ticketPrice: 0.037, // ETH price
-  },
-  {
-    id: 2,
-    name: "Blockchain Expo",
-    description: "The leading blockchain expo in Europe.",
-    date: "2025-07-15",
-    location: "Olympia, London",
-    ticketPrice: 0.037, // ETH price
-  },
-  {
-    id: 3,
-    name: "Crypto Summit",
-    description: "A summit for cryptocurrency enthusiasts and professionals.",
-    date: "2025-08-20",
-    location: "Marina Bay Sands, Singapore",
-    ticketPrice: 0.037, // ETH price
-  },
-  {
-    id: 4,
-    name: "NFT Art Fair",
-    description: "An art fair showcasing the latest in NFT art.",
-    date: "2025-09-05",
-    location: "Javits Center, New York",
-    ticketPrice: 0.03, // ETH price
-  },
-  {
-    id: 5,
-    name: "DeFi Conference",
-    description: "A conference dedicated to decentralized finance.",
-    date: "2025-10-12",
-    location: "Palais des Congrès, Paris",
-    ticketPrice: 0.03, // ETH price
-  },
-];
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const events = [
+    {
+      id: 1,
+      name: "Web 3 Conference",
+      description: "A conference about the future of Web 3.0 technologies.",
+      date: "Friday, June 10, 2025",
+      location: "Atlantis the Palm, Dubai",
+      ticketPrice: 0.037, // ETH price
+    },
+    {
+      id: 2,
+      name: "Blockchain Expo",
+      description: "The leading blockchain expo in Europe.",
+      date: "Tuesday, July 15, 2025",
+      location: "Olympia, London",
+      ticketPrice: 0.037, // ETH price
+    },
+    {
+      id: 3,
+      name: "Crypto Summit",
+      description: "A summit for cryptocurrency enthusiasts and professionals.",
+      date: "Wednesday, August 20, 2025",
+      location: "Marina Bay Sands, Singapore",
+      ticketPrice: 0.037, // ETH price
+    },
+    {
+      id: 4,
+      name: "NFT Art Fair",
+      description: "An art fair showcasing the latest in NFT art.",
+      date: "Friday, September 5, 2025",
+      location: "Javits Center, New York",
+      ticketPrice: 0.03, // ETH price
+    },
+    {
+      id: 5,
+      name: "DeFi Conference",
+      description: "A conference dedicated to decentralized finance.",
+      date: "Sunday, October 12, 2025",
+      location: "Palais des Congrès, Paris",
+      ticketPrice: 0.03, // ETH price
+    },
+  ];
+  const router = useRouter();
   return (
     <div className="container mx-auto py-10">
-      <Heading title="Upcoming Event" />
+      <Heading title="Upcoming Events" />
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {events.map((item) => (
           <Card key={item.id} className="">
@@ -67,7 +70,9 @@ export default function Home() {
               <p>{item.location}</p>
             </CardContent>
             <CardFooter>
-              <Button>View Details</Button>
+              <Button onClick={() => router.push(`/event/${item.id}`)}>
+                View Details
+              </Button>
             </CardFooter>
           </Card>
         ))}
