e57fe4639dba88e817a4ea254c2ea7c57d8dc3bb
I504428 <gabriel.azambuja@sap.com>
2025-02-19 19:47:20 -0300

    feat: update event factory contract

--- backend/contracts/EventFactory.sol
+++ backend/contracts/EventFactory.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.20;
 
 contract EventFactory {
-
     string[] public categories = [
         "IT & Technology", "Music", "Festivals", "Sports", "Business & Entrepreneurship",
         "Education & Learning", "Health & Wellness", "Arts & Culture", "Food & Beverage", "Science & Innovation",
@@ -15,36 +14,42 @@ contract EventFactory {
     struct Event {
         uint256 eventId;
         string metadataCID;
+        uint256 totalTickets;
+        uint256 soldTickets;
         address organizer;
     }
 
     uint256 private nextEventId;
     mapping(uint256 => Event) public events;
 
-    event EventCreated(
-        uint256 indexed eventId,
-        string metadataCID,
-        address indexed organizer
-    );
+    event EventCreated(uint256 indexed eventId, string metadataCID, uint256 totalTickets, address indexed organizer);
+    event TicketsSold(uint256 indexed eventId, uint256 ticketsSold);
 
-    function createEvent(
-        string memory _metadataCID,
+    function createEvent(string memory _metadataCID, uint256 _totalTickets) external {
+        require(_totalTickets > 0, "Total tickets must be greater than zero");
 
-    ) external {
-        events[nextEventId] = Event(
-            nextEventId,
-            _metadataCID,
-            msg.sender
-        );
-        emit EventCreated(nextEventId, _metadataCID, msg.sender);
+        events[nextEventId] = Event({
+            eventId: nextEventId,
+            metadataCID: _metadataCID,
+            totalTickets: _totalTickets,
+            soldTickets: 0,
+            organizer: msg.sender
+        });
+
+        emit EventCreated(nextEventId, _metadataCID, _totalTickets, msg.sender);
         nextEventId++;
     }
 
-    function getNextEventId() external view returns (uint256) {
-        return nextEventId;
+    function updateSoldTickets(uint256 eventId, uint256 amount) external {
+        require(eventId < nextEventId, "Event does not exist");
+        require(events[eventId].soldTickets + amount <= events[eventId].totalTickets, "Not enough tickets available");
+
+        events[eventId].soldTickets += amount;
+        emit TicketsSold(eventId, amount);
     }
 
-    function getCategories() external view returns (string[] memory) {
-        return categories;
+    function getEventDetails(uint256 eventId) external view returns (uint256, uint256, uint256, address) {
+        Event storage e = events[eventId];
+        return (e.eventId, e.totalTickets, e.soldTickets, e.organizer);
     }
 }
