62a51595ec7c7055ac94a638f1138115b78b3f78
I504428 <gabriel.azambuja@sap.com>
2025-02-20 20:56:23 -0300

    feat: add smart contract interface

---
+++ backend/contracts/interfaces/IEventFactory.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+interface IEventFactory {
+    function getEventDetails(
+        uint256 _eventId
+    )
+        external
+        view
+        returns (
+            uint256 eventId,
+            uint256 totalTickets,
+            uint256 soldTickets,
+            address organizer,
+            bool canceled
+        );
+
+    function updateSoldTickets(uint256 eventId, uint256 amount) external;
+}
