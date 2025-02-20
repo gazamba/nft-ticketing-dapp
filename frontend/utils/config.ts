e9c855620eda06e80513636f00556c0f0b7b3c46
I504428 <gabriel.azambuja@sap.com>
2025-02-20 08:54:19 -0300

    feat: update pinata env variable

--- frontend/utils/config.ts
+++ frontend/utils/config.ts
@@ -1,9 +1,8 @@
 "server only";
 
 import { PinataSDK } from "pinata-web3";
-import { env } from "process";
 
 export const pinata = new PinataSDK({
-  pinataJwt: `${process.env.PINATA_JWT}`,
-  pinataGateway: `${process.env.PINATA_JWT_TOKEN}`,
+  pinataJwt: `${process.env.NEXT_PUBLIC_PINATA_JWT_TOKEN}`,
+  pinataGateway: `${process.env.NEXT_PUBLIC_PINATA_PUBLIC_GATEWAY_URL}`,
 });
