8a3ba2d4914869596dc2c8eb2d124a70897b5f16
I504428 <gabriel.azambuja@sap.com>
2025-02-21 19:08:01 -0300

    feat: update api to handle json

--- frontend/app/api/files/route.tsx
+++ frontend/app/api/files/route.tsx
@@ -3,10 +3,19 @@ import { pinata } from "@/utils/config";
 
 export async function POST(request: NextRequest) {
   try {
-    const data = await request.formData();
-    const file: File | null = data.get("file") as unknown as File;
-    const uploadData = await pinata.upload.file(file);
-    const url = await pinata.gateways.convert(uploadData.IpfsHash);
+    // const data = await request.formData();
+    // const file: File | null = data.get("file") as unknown as File;
+    // const uploadData = await pinata.upload.file(file);
+    // const url = await pinata.gateways.convert(uploadData.IpfsHash);
+
+    const data = await request.json();
+    const upload = await pinata.upload.json({
+      content: data,
+      name: "TO BE DEFINED",
+    });
+    const url = await pinata.gateways.convert(upload.IpfsHash);
+    // url = "IpfsHash": "QmXLM5D33rzCogZVtL9Qz8fJbnaT74SPaZyMv5cxSL4ds3" = CID
+
     return NextResponse.json(url, { status: 200 });
   } catch (e) {
     console.log(e);
