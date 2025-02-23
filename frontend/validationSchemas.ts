18e8e24cab4518e10a061d7349dc016e078de038
I504428 <gabriel.azambuja@sap.com>
2025-02-23 17:36:03 -0300

    feat: update schema validations

--- frontend/validationSchemas.ts
+++ frontend/validationSchemas.ts
@@ -1,13 +1,13 @@
 import zod from "zod";
 
 export const eventSchema = zod.object({
-  id: zod.number(),
+  id: zod.number().optional(),
   name: zod.string().min(1, "Name is required").max(255),
   description: zod.string().min(1, "Description is required").max(65535),
   date: zod.date(),
-  location: zod.string(),
-  ticketPrice: zod.coerce.number(),
-  totalTickets: zod.coerce.number(),
+  location: zod.string().min(1, "Location is required").max(255),
+  ticketPrice: zod.coerce.number().min(1, "Number must be greater than 0"),
+  totalTickets: zod.coerce.number().min(1, "Number must be greater than 0"),
 });
 
 export const nftSchema = zod.object({
