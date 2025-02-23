6b42975c756c06dc5865748cba44b5dbacc0e1b1
I504428 <gabriel.azambuja@sap.com>
2025-02-22 21:55:42 -0300

    feat: add console script to test smart contract functions

---
+++ backend/scripts/console.js
@@ -0,0 +1,35 @@
+const {ethers} = require("ethers")
+
+async function main() {
+
+  // GET Transaction Hash -> low-level blockchain function
+  // const txHash = "0x9be21f33ad94dd9459d08cefbd860d8c43a59fe7d3a555d84855511db785c30c";
+  // const provider = hre.ethers.provider;
+  // const tx = await provider.send("eth_getTransactionByHash", [txHash]);
+  // console.log("Transaction Details:", tx);
+
+  // GET NFT Owner
+  // const provider = new ethers.JsonRpcProvider("http://127.0.0.1:8545")
+  // const ticketNFTABI = ["function owner() view returns (address)"];
+  // const ticketNFTAddress = "0x5fbdb2315678afecb367f032d93f642f64180aa3"
+  // const ticketNFT = new ethers.Contract(ticketNFTAddress, ticketNFTABI, provider)
+  // const owner = await ticketNFT.owner();
+  // console.log(owner)
+
+  // Mint Ticket NFT
+  const provider = new ethers.JsonRpcProvider("http://127.0.0.1:8545");
+  const addressTo = "0x70997970C51812dc3A010C7d01b50e0d17dc79C8"
+  const 
+
+  address to,
+  string memory tokenURI,
+  uint256 eventId
+
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
