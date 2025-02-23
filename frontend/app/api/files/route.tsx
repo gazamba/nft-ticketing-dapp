a28cb960962ecaf4b3d5117dc23b3d933a578e3c
I504428 <gabriel.azambuja@sap.com>
2025-02-23 19:01:36 -0300

    feat: add serverless api routes

--- frontend/app/api/files/route.tsx
+++ frontend/app/api/files/route.tsx
@@ -1,19 +1,44 @@
 import { NextResponse, type NextRequest } from "next/server";
 import { pinata } from "@/utils/config";
+import { url } from "inspector";
 
 export async function POST(request: NextRequest) {
   try {
+    // TODO: PENDING SECURE ENDPOINT
+
+    // single file
     // const data = await request.formData();
     // const file: File | null = data.get("file") as unknown as File;
     // const uploadData = await pinata.upload.file(file);
     // const url = await pinata.gateways.convert(uploadData.IpfsHash);
 
-    const data = await request.json();
-    const upload = await pinata.upload.json({
-      content: data,
-      name: "TO BE DEFINED",
+    // Metadata Event
+    // {
+    //   id: 1,
+    //   name: "Web 3 Conference",
+    //   description: "A conference about the future of Web 3.0 technologies.",
+    //   date: "Friday, June 10, 2025",
+    //   location: "Atlantis the Palm, Dubai",
+    //   ticketPrice: 0.037, // ETH price
+    // },
+
+    // Two files to created a folder is required.
+    const file1 = new File([JSON.stringify({ name: "" })], "1.json", {
+      type: "application/json",
     });
-    const url = await pinata.gateways.convert(upload.IpfsHash);
+
+    const file2 = new File(["hello world again!"], "2.json", {
+      type: "application/json",
+    });
+    const upload = await pinata.upload.fileArray([file1, file2]);
+
+    // Json files to build and upload
+    // const data = await request.json();
+    // const upload = await pinata.upload.json({
+    //   content: data,
+    //   name: "TO BE DEFINED",
+    // });
+    // const url = await pinata.gateways.convert(upload.IpfsHash);
     // url = "IpfsHash": "QmXLM5D33rzCogZVtL9Qz8fJbnaT74SPaZyMv5cxSL4ds3" = CID
 
     return NextResponse.json(url, { status: 200 });
---
+++ frontend/app/api/nft-metadata/route.tsx
@@ -0,0 +1,17 @@
+import { NextRequest } from "next/server";
+import { pinata } from "@/utils/config";
+
+export function POST(req: NextRequest) {
+  //   const metadata = {
+  //     name: "Ticket #1",
+  //     description: "Admission to Event 42",
+  //     image: `https://gateway.pinata.cloud/ipfs/${imageCid}`,
+  //     attributes: [
+  //       { trait_type: "Event ID", value: "42" },
+  //       { trait_type: "Date", value: "2025-02-22" },
+  //       { trait_type: "Location", value: "Atlantis the Palm, Dubai" },
+  //       { "trait_type": "Ticket Price ETH", "value": "" },
+  //     ],
+  //   };
+  // create schema global
+}
