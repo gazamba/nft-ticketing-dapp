ab8ca55aa0260a96a9690371b0b8c566bcb66566
I504428 <gabriel.azambuja@sap.com>
2025-03-02 21:43:38 -0300

    feat: update env variable

--- frontend/components/client-wrapper.tsx
+++ frontend/components/client-wrapper.tsx
@@ -12,9 +12,7 @@ import ToastProvider from "@/components/providers/toast-provider";
 const config = createConfig({
   chains: [sepolia, localhost],
   transports: {
-    [sepolia.id]: http(
-      `https://sepolia.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_KEY}`
-    ),
+    [sepolia.id]: http(process.env.INFURA_RPC_URL),
     [localhost.id]: http("http://127.0.0.1:8545"),
   },
   connectors: [metaMask()],
