48f7ca67ffe8565951f773da9b3cc7a95edf1e35
I504428 <gabriel.azambuja@sap.com>
2025-03-02 21:40:56 -0300

    feat: update schema validation

--- frontend/validationSchemas.ts
+++ frontend/validationSchemas.ts
@@ -1,10 +1,10 @@
 import zod from "zod";
 
 export const eventSchema = zod.object({
-  id: zod.number().optional(),
+  eventId: zod.number().optional(),
   name: zod.string().min(1, "Name is required").max(255),
   description: zod.string().min(1, "Description is required").max(65535),
-  date: zod.date(),
+  date: zod.coerce.date(),
   location: zod.string().min(1, "Location is required").max(255),
   ticketPrice: zod.coerce.number().min(1, "Number must be greater than 0"),
   totalTickets: zod.coerce.number().min(1, "Number must be greater than 0"),
