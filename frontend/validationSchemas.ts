7044c783afa850ed8ba8b234993b779467bd7777
I504428 <gabriel.azambuja@sap.com>
2025-03-05 18:37:20 -0300

    feat: add category validation

--- frontend/validationSchemas.ts
+++ frontend/validationSchemas.ts
@@ -4,6 +4,7 @@ export const eventSchema = zod.object({
   eventId: zod.number().optional(),
   name: zod.string().min(1, "Name is required").max(255),
   description: zod.string().min(1, "Description is required").max(65535),
+  category: zod.string().min(1, "Category is required").max(255),
   date: zod.coerce.date(),
   location: zod.string().min(1, "Location is required").max(255),
   ticketPrice: zod.coerce.number().min(1, "Number must be greater than 0"),
