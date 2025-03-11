31fc66dc9529c6ef3a89c7bb5273f488bc7357dd
I504428 <gabriel.azambuja@sap.com>
2025-03-10 22:01:10 -0300

    feat: add api for handling ticket nft metadata

--- frontend/app/api/events/[eventId]/tickets/metadata/route.tsx
+++ frontend/app/api/events/[eventId]/tickets/metadata/route.tsx
@@ -1,17 +1,45 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
+import { ticketNFTSchema } from "@/validationSchemas";
 import { pinata } from "@/utils/config";
 
-export function POST(req: NextRequest) {
-  //   const metadata = {
-  //     name: "Ticket #1",
-  //     description: "Admission to Event 42",
-  //     image: `https://gateway.pinata.cloud/ipfs/${imageCid}`,
-  //     attributes: [
-  //       { trait_type: "Event ID", value: "42" },
-  //       { trait_type: "Date", value: "2025-02-22" },
-  //       { trait_type: "Location", value: "Atlantis the Palm, Dubai" },
-  //       { "trait_type": "Ticket Price ETH", "value": "" },
-  //     ],
-  //   };
-  // create schema global
+export async function POST(
+  req: NextRequest,
+  { params }: { params: { eventId: string } }
+) {
+  const { tickets, pinataGroupId } = await req.json();
+  // const {tickets} = ticketNFTSchema.array().parse(body)
+  try {
+    const ticketCIDs = [];
+    for (const ticket of tickets) {
+      const metadata = {
+        name: ticket.name,
+        description: `Admission to Event ${params.eventId}`,
+        image: `https://gateway.pinata.cloud/ipfs/fakeIdForNow`,
+        attributes: [
+          { trait_type: "Event ID", value: params.eventId },
+          { trait_type: "Token ID", value: ticket.tokenId },
+        ],
+      };
+      const upload = await pinata.upload
+        .json(metadata)
+        .addMetadata({
+          name: `event-${params.eventId}-ticket-${ticket.tokenId}.json`,
+        })
+        .group(pinataGroupId);
+
+      ticketCIDs.push(upload.IpfsHash);
+    }
+
+    return NextResponse.json(
+      { tokenId: tickets.map((t: { tokenId: any }) => t.tokenId), ticketCIDs },
+      { status: 201 }
+    );
+  } catch (error) {
+    console.log(error);
+
+    return NextResponse.json(
+      { error: `Internal Server Error: ${error}` },
+      { status: 500 }
+    );
+  }
 }
