5522e10218f4d782e84e526bcd2eeb8482f9ca49
I504428 <gabriel.azambuja@sap.com>
2025-03-10 21:57:53 -0300

    feat: update schema validation

--- frontend/validationSchemas.ts
+++ frontend/validationSchemas.ts
@@ -1,16 +1,22 @@
-import zod from "zod";
+import * as z from "zod";
 
-export const eventSchema = zod.object({
-  eventId: zod.number().optional(),
-  name: zod.string().min(1, "Name is required").max(255),
-  description: zod.string().min(1, "Description is required").max(65535),
-  category: zod.string().min(1, "Category is required").max(255),
-  date: zod.coerce.date(),
-  location: zod.string().min(1, "Location is required").max(255),
-  ticketPrice: zod.coerce.number().min(1, "Number must be greater than 0"),
-  totalTickets: zod.coerce.number().min(1, "Number must be greater than 0"),
+export const eventFormSchema = z.object({
+  name: z.string().min(1, "Name is required").max(255),
+  description: z.string().min(1, "Description is required").max(65535),
+  category: z.string().min(1, "Category is required").max(255),
+  date: z.coerce.date(),
+  location: z.string().min(1, "Location is required").max(255),
+  ticketPrice: z.coerce.number().min(1, "Number must be greater than 0"),
+  totalTickets: z.coerce.number().min(1, "Number must be greater than 0"),
 });
 
-export const nftSchema = zod.object({
-  // TBD
+export const eventSchema = eventFormSchema.extend({
+  eventId: z.number().optional(),
+  pinataGroupId: z.string().optional(),
+  metadataCID: z.string().optional(),
+});
+
+export const ticketNFTSchema = z.object({
+  tokenId: z.number().min(1, "Token ID must be greater than 0"),
+  name: z.string().min(1, "Name is required").max(255).optional(),
 });
